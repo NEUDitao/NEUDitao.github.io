6150af4734b6a1df73c4de45d758416ad3c935c6
NEUDitao <li.edwa@husky.neu.edu>
2020-03-24 00:22:53 -0400

    Updates

---
+++ precache-manifest.f081dbb9a6997c7355c0a475bdf674bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a73e9c520a682cdee7f6223bcc6697b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88274db72b195c9333e0",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e1957c5590ef7742cd58",
+    "url": "/static/css/main.b0cbd609.chunk.css"
+  },
+  {
+    "revision": "88274db72b195c9333e0",
+    "url": "/static/js/2.e09dd519.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.e09dd519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1957c5590ef7742cd58",
+    "url": "/static/js/main.a6d9f00f.chunk.js"
+  },
+  {
+    "revision": "b501012cb1c7196c8d11",
+    "url": "/static/js/runtime-main.a5564a1c.js"
+  }
+]);
